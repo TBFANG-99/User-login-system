036d8610d096a454b3962e6b620bc90e5bb31cad
tbfang <99115154@qq.com>
2020-07-28 10:38:05 +0800

    主函数

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "userTree.h"
+#include"Node.h"
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<string>
+#include"menu.h"
+using namespace std;
+int main()
+{
+	menu a;
+	a.mainmenu();
+}
